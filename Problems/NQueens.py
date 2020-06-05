0e4434f649f8e84c76d8712f81deb998e34fa1cb
Kamil-Jan <kamilyakubov80@gmail.com>
2020-06-05 18:58:43 +0300

    modified main function to generator

--- Problems/NQueens.py
+++ Problems/NQueens.py
@@ -2,30 +2,30 @@ def is_safe(board, row, col):
     n = len(board)
     for i in range(n):
         # check column
-        if board[i][col] == 1:
+        if board[i][col] == "Q":
             return False
         # check diagonals
         for j in range(n):
             if i + j == row + col or i - j == row - col:
-                if board[i][j] == 1:
+                if board[i][j] == "Q":
                     return False
     return True
 
-def find_solution(board, row, queen_left):
-    if queen_left == 0:
-        return True
-    n = len(board)
-    for i in range(n):
-        if is_safe(board, row, i):
-            board[row][i] = 1
-            if find_solution(board, row + 1, queen_left - 1):
-                return True
-            board[row][i] = "_"
-    return False
+def find_solutions(board, row, queen_left, solution_count=0):
+    if row == len(board):
+        yield board
+    else:
+        n = len(board)
+        for i in range(n):
+            if is_safe(board, row, i):
+                board[row][i] = "Q"
+                yield from find_solutions(board, row + 1, queen_left - 1)
+                board[row][i] = "."
 
 if __name__ == "__main__":
-    board = [["_" for _ in range(8)] for _ in range(8)]
-    find_solution(board, 0, len(board))
-    for row in board:
+    N = 8
+    b = [["." for _ in range(N)] for _ in range(N)]
+    start = time.time()
+    for row in next(find_solutions(b, 0, len(b))):
         print(*row)
 
